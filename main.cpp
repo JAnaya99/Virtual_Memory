980aa3e394009f6defa16157b74b91f40c1ee965
Ulises <uliseslf99@hotmail.com>
2020-06-14 18:58:17 -0500

    Using system methods in corresponding instructions from input

--- main.cpp
+++ main.cpp
@@ -23,34 +23,46 @@ int main(){
             virtual_memory::Instruction instruction = current_instruction.GetData();
             if(current_type == virtual_memory::InstructionType::load){
                 // Load a process
+                system.UploadProcess(instruction.process(), instruction.numBytes());
+                // TODO Quitar banderas para debuggear
                 cout << "Instruccion de carga" << endl;
                 cout << instruction.numBytes() << ", process: " << instruction.process() << endl;
             }
             else if(current_type == virtual_memory::InstructionType::access){
                 // Access to address
+                system.AccessVirtualMemory(instruction.process(), instruction.address(), instruction.mode());
+                // TODO Quitar banderas para debuggear
                 cout << "Instruccion de acceso" << endl;
                 cout << instruction.address() << ", process: " << instruction.process() << ", mode= " << instruction.mode() << endl;
             }
             else if(current_type == virtual_memory::InstructionType::free){
                 // Free pages
+                system.FreeProcess(instruction.process());
+                // TODO Quitar banderas para debuggear
                 cout << "Instruccion de liberar" << endl;
                 cout << "process: " << instruction.process() << endl;
             }
             else if(current_type == virtual_memory::InstructionType::comment){
                 // Add comment
+                // TODO Output comment?
+                // TODO Quitar banderas para debuggear
                 cout << "Instruccion de comentario" << endl;
                 cout << "Comment: " << instruction.message() << endl;
             }
             else if(current_type == virtual_memory::InstructionType::end){
                 // End of the request set
+                system.RestartMemory();
+                // TODO Quitar banderas para debuggear
                 cout << "Instruccion de termino de conjunto" << endl;
             }
             else if(current_type == virtual_memory::InstructionType::exit){
                 // Exit program
+                // TODO Quitar banderas para debuggear
                 cout << "Instruccion de salida" << endl;
             }
             else {
                 // Output ignoring type
+                // TODO Output invalid instructions message
                 cout << "Instruccion incorrecta" << endl;
                 cout << "Message: " << instruction.message() << endl;
             }
